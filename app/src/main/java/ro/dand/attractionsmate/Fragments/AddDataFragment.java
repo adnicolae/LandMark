ac398484037d37db906c0e35b06746357f7ab40c
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-09-20 18:11:42 +0300

    Updated insert location functionality

--- app/src/main/java/ro/dand/attractionsmate/Fragments/AddDataFragment.java
+++ app/src/main/java/ro/dand/attractionsmate/Fragments/AddDataFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -19,7 +21,8 @@ import ro.dand.attractionsmate.R;
 public class AddDataFragment extends Fragment {
     MarkerInfoRepository markerInfoRepository = new MarkerInfoRepository();
     ArrayList<MarkerInfo> markerInfo = markerInfoRepository.getAllMarkerInfo();
-
+    EditText locationTitle, locationShortDescription, locationLat, locationLng, locationLongDescription;
+    Button insertBtn;
 
     public AddDataFragment() {
         // Required empty public constructor
@@ -28,15 +31,46 @@ public class AddDataFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        markerInfo.add(new MarkerInfo("TestMarker", "Test", new LatLng(44.450951,26.235211), "Teeeest", R.drawable.vacaresti));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_data, container, false);
+
+        locationTitle = (EditText) view.findViewById(R.id.locationName);
+        locationShortDescription = (EditText) view.findViewById(R.id.shortDescription);
+        locationLat = (EditText) view.findViewById(R.id.locationLat);
+        locationLng = (EditText) view.findViewById(R.id.locationLng);
+        locationLongDescription = (EditText) view.findViewById(R.id.longDescription);
+        insertBtn = (Button) view.findViewById(R.id.insertBtn);
+        insertLocation();
+
+        return view;
     }
 
+    public void insertLocation() {
+        try {
+            insertBtn.setOnClickListener(
+                    new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            markerInfo.add(
+                                    new MarkerInfo(
+                                            locationTitle.getText().toString(),
+                                            locationShortDescription.getText().toString(),
+                                            new LatLng(Double.parseDouble(locationLat.getText().toString()), Double.parseDouble(locationLng.getText().toString())),
+                                            locationLongDescription.getText().toString(),
+                                            R.drawable.no_preview
+                                    ));
+                        }
+                    }
+            );
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
